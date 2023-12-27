cf73c17290ed831f559f8d96ff8297eec97c89c5
daisuke-kikukawa <58897760+DaisukeKikukawa@users.noreply.github.com>
2023-12-27 10:34:22 +0900

    Update B-Break Number.js

--- Atcoder/基本10問/第三問/B-Break Number.js
+++ Atcoder/基本10問/第三問/B-Break Number.js
@@ -1,1 +1,24 @@
+function main(input) {
+  const args = input.split("\n");
+  const nums = args[0].split(" ");
+  const N = parseInt(nums[0], 10);
+    let maxDivCount = 0;  // 最大の割れる回数
+    let result = 1;       // 最も多く割れる数
 
+    for (let i = 1; i <= N; i++) {
+        let count = 0;  // 現在の数の割れる回数
+        let num = i;
+        while (num % 2 === 0) {
+            count++;
+            num /= 2;
+        }
+        if (count > maxDivCount) {
+            maxDivCount = count;
+            result = i;
+        }
+    }
+
+    console.log(result);
+}
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
