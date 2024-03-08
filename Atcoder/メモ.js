dd077c3a86ede6e040ad9abb801871286695b204
daisuke-kikukawa <58897760+DaisukeKikukawa@users.noreply.github.com>
2024-03-08 17:17:09 +0900

    Update メモ.js

--- Atcoder/メモ.js
+++ Atcoder/メモ.js
@@ -7,3 +7,13 @@ str.substr( 開始位置, 文字数 );
 ## 引数一つの場合
 / 文字数を省略して、末尾まですべてを抽出する
 var result = str.substr( 5 );
+
+## sliceメソッド
+let str = "abcdefg";
+let subStr = str.slice(1, -1); // 1番目のインデックスから始まり、最後のインデックスの1つ前まで取得します。
+console.log(subStr); // "bcdef"
+
+## substringメソッド
+let str = "abcdefg";
+let subStr = str.substring(1, str.length - 1); // 1番目のインデックスから始まり、最後のインデックスの1つ前まで取得します。
+console.log(subStr); // "bcdef"
