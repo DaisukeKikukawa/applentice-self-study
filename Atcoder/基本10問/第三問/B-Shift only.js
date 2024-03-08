55172b94c57a5e2cfa6875264aa3c6dd5898e539
daisuke-kikukawa <58897760+DaisukeKikukawa@users.noreply.github.com>
2024-03-08 23:15:23 +0900

    Update B-Shift only.js

--- Atcoder/基本10問/第三問/B-Shift only.js
+++ Atcoder/基本10問/第三問/B-Shift only.js
@@ -1,29 +1,24 @@
 function main(input) {
   const args = input.split("\n");
-  // console.log(args); // [ '3 4', '5', '' ]
   const nums = args[0].split(" ");
-  const nums1 = args[1].split(" ");
-  // console.log(nums)  // [ '3', '4' ]
-  // console.log(nums1)
-  // const num = parseInt(nums[0], 10);
-  // console.log(num) // [3]
   const N = parseInt(nums[0], 10);
-  const A1 = parseInt(nums1[0], 10);
-  const A2 = parseInt(nums1[1], 10);
-  const A3 = parseInt(nums1[2], 10);
-  
-  let numbers = [];
+  const a = args[1].split(" ").map((n) => parseInt(n, 10));
   let count = 0;
-  for (let i = 0; i <=N-1;i++) {
-    numbers.push(parseInt(nums1[i], 10));
-  }
-  // console.log(numbers)
-  while(numbers.every(num => num % 2 === 0)) {
-    for (let i = 0;i<numbers.length;i++) {
-      numbers[i] = numbers[i] / 2
+  
+  while (true) {
+    let exist_odd = false;
+    for (let i = 0; i < N; i++) {
+      if(a[i] % 2 !== 0) {
+        exist_odd = true;
+      }
+    }
+    if (exist_odd) break;
+    for (let i = 0; i < N; i++) {
+      a[i] /= 2; 
     }
     count++;
   }
+
   console.log(count)
 }
 
