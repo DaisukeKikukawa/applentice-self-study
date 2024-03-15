5749f9336084b6e7ced92f27232e7d9639b4753e
daisuke-kikukawa <58897760+DaisukeKikukawa@users.noreply.github.com>
2024-03-15 18:15:33 +0900

    Update B - Some Sums.js

--- Atcoder/基本10問/第五問/B - Some Sums.js
+++ Atcoder/基本10問/第五問/B - Some Sums.js
@@ -1,22 +1,30 @@
-function sumOfDigits(n) {
+function main(input) {
+  const args = input.split('\n');
+  const nums = args[0].split(' ');
+  const N = parseInt(nums[0], 10);
+  const A = parseInt(nums[1], 10);
+  const B = parseInt(nums[2], 10);
+  
+  function allsum(num){
+    //String型に変換
+    num = num.toString();
+    //各桁の和を表す変数sumを定義
     let sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n = Math.floor(n / 10);
+    //文字列のi番目を取り出し、sumに足していく
+    for(let i=0; i < num.length; i++){
+    sum = sum + parseInt(num[i])
+  }
+  return parseInt(sum);
+}
+  
+  let result = 0;
+  for (let i = 1; i <=  N; i++) {
+     let sum = allsum(i);
+     if (sum >= A && sum <= B) {
+       result += i;
+     }
     }
-    return sum;
+  console.log(result);
 }
 
-function solve(N, A, B) {
-    let total = 0;
-    for (let i = 1; i <= N; i++) {
-        const sum = sumOfDigits(i);
-        if (sum >= A && sum <= B) {
-            total += i;
-        }
-    }
-    return total;
-}
-
-// 例: N = 20, A = 2, B = 5 の場合
-console.log(solve(20, 2, 5));
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
